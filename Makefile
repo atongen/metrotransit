ae3f804e2af35b102f36d4ef669f81d9551d3a0d
Andrew Tongen <atongen@gmail.com>
2019-04-12 17:57:48 -0500

    Autogenerate index

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
 NAME=metrotransit
 
-.PHONY: default build webpack test clean all
+.PHONY: default build webpack test clean prod all
 
 default: all
+
+clean:
+	npm run-script clean
 
 build:
 	npm run-script build
 
-webpack:
-	npm run-script webpack
-
 test: build
 	npm run-script test
 
-clean:
-	npm run-script clean
+webpack:
+	npm run-script webpack
+
+prod: clean build
+	npm run-script webpack:production
 
 all: build webpack
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require("path");
 
 const isProd = process.env.NODE_ENV === "production";
-const webpack = require('webpack');
-const CompressionPlugin = require('compression-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     plugins: [
         new webpack.HashedModuleIdsPlugin(),
-        new CompressionPlugin(),
-        new ManifestPlugin({})
+        new CompressionPlugin({
+            test: /\.js(\?.*)?$/i
+        }),
+        new ManifestPlugin({}),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html.ejs",
+            filename: "../index.html"
+        })
     ],
     entry: {
         Index: "./lib/js/src/Index.js",
@@ -18,7 +25,7 @@ module.exports = {
     mode: isProd ? "production" : "development",
     output: {
         path: path.join(__dirname, "public/assets"),
-        filename: isProd ? "[name].[contenthash].js" : "[name].js",
+        filename: "[name].[contenthash].js",
     },
     optimization: {
         minimizer: isProd ? [new UglifyJsPlugin({sourceMap: true})] : [],
@@ -27,8 +34,8 @@ module.exports = {
             cacheGroups: {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
+                    name: "vendors",
+                    chunks: "all"
                 }
             }
         }
